95999f478d56256b2b9403ec3b78ca2723d785b7
YoSiem <patryk.olejnik655@gmail.com>
2026-05-10 21:01:20 +0200

    refactor: SummonsActionsControl uses Designer-defined TableLayoutPanel layout

--- src/App.WinForms/Controls/SummonsActionsControl.Designer.cs
+++ src/App.WinForms/Controls/SummonsActionsControl.Designer.cs
@@ -1,8 +1,12 @@
 namespace App.WinForms.Controls;
 
+using System.Drawing;
+using System.Windows.Forms;
+
 partial class SummonsActionsControl
 {
     private System.ComponentModel.IContainer components = null;
+    private TableLayoutPanel _tlpRoot;
     private GroupBox gbSummonActions;
     private TableLayoutPanel tlpRoot;
     private GroupBox gbAddSummon;
@@ -33,6 +37,7 @@ partial class SummonsActionsControl
 
     private void InitializeComponent()
     {
+        _tlpRoot = new TableLayoutPanel();
         gbSummonActions = new GroupBox();
         tlpRoot = new TableLayoutPanel();
         gbAddSummon = new GroupBox();
@@ -50,6 +55,8 @@ partial class SummonsActionsControl
         lblStage = new Label();
         nudStage = new NumericUpDown();
         btnStageSummon = new Button();
+
+        _tlpRoot.SuspendLayout();
         gbSummonActions.SuspendLayout();
         tlpRoot.SuspendLayout();
         gbAddSummon.SuspendLayout();
@@ -60,231 +67,160 @@ partial class SummonsActionsControl
         tlpStage.SuspendLayout();
         ((System.ComponentModel.ISupportInitialize)nudStage).BeginInit();
         SuspendLayout();
-        // 
+
+        // _tlpRoot
+        _tlpRoot.Dock = DockStyle.Fill;
+        _tlpRoot.ColumnCount = 1;
+        _tlpRoot.RowCount = 1;
+        _tlpRoot.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+        _tlpRoot.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+        _tlpRoot.Controls.Add(gbSummonActions, 0, 0);
+
         // gbSummonActions
-        // 
-        gbSummonActions.Controls.Add(tlpRoot);
-        gbSummonActions.Dock = DockStyle.Fill;
-        gbSummonActions.Location = new Point(0, 0);
-        gbSummonActions.Name = "gbSummonActions";
-        gbSummonActions.Size = new Size(430, 260);
-        gbSummonActions.TabIndex = 0;
-        gbSummonActions.TabStop = false;
         gbSummonActions.Text = "Summon Commands";
-        // 
+        gbSummonActions.AutoSize = false;
+        gbSummonActions.Padding = new Padding(8, 18, 8, 8);
+        gbSummonActions.Dock = DockStyle.Fill;
+        gbSummonActions.Controls.Add(tlpRoot);
+
         // tlpRoot
-        // 
+        tlpRoot.Dock = DockStyle.Fill;
         tlpRoot.ColumnCount = 1;
+        tlpRoot.RowCount = 2;
         tlpRoot.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+        tlpRoot.RowStyles.Add(new RowStyle(SizeType.Absolute, 110F));
+        tlpRoot.RowStyles.Add(new RowStyle(SizeType.Absolute, 140F));
         tlpRoot.Controls.Add(gbAddSummon, 0, 0);
         tlpRoot.Controls.Add(gbStageSummon, 0, 1);
-        tlpRoot.Dock = DockStyle.Fill;
-        tlpRoot.Location = new Point(3, 23);
-        tlpRoot.Name = "tlpRoot";
-        tlpRoot.RowCount = 2;
-        tlpRoot.RowStyles.Add(new RowStyle());
-        tlpRoot.RowStyles.Add(new RowStyle());
-        tlpRoot.Size = new Size(424, 234);
-        tlpRoot.TabIndex = 0;
-        // 
+
         // gbAddSummon
-        // 
-        gbAddSummon.Controls.Add(tlpAdd);
-        gbAddSummon.Dock = DockStyle.Fill;
-        gbAddSummon.Location = new Point(3, 3);
-        gbAddSummon.Name = "gbAddSummon";
-        gbAddSummon.Size = new Size(418, 104);
-        gbAddSummon.TabIndex = 0;
-        gbAddSummon.TabStop = false;
         gbAddSummon.Text = "Add Summon";
-        // 
+        gbAddSummon.AutoSize = false;
+        gbAddSummon.Padding = new Padding(8, 18, 8, 8);
+        gbAddSummon.Dock = DockStyle.Fill;
+        gbAddSummon.Controls.Add(tlpAdd);
+
         // tlpAdd
-        // 
-        tlpAdd.ColumnCount = 4;
-        tlpAdd.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 80F));
-        tlpAdd.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 45F));
-        tlpAdd.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 110F));
-        tlpAdd.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 55F));
+        tlpAdd.Dock = DockStyle.Fill;
+        tlpAdd.ColumnCount = 3;
+        tlpAdd.RowCount = 2;
+        tlpAdd.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 130F));
+        tlpAdd.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+        tlpAdd.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 140F));
+        tlpAdd.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+        tlpAdd.RowStyles.Add(new RowStyle(SizeType.Absolute, 36F));
         tlpAdd.Controls.Add(lblSummonId, 0, 0);
         tlpAdd.Controls.Add(nudSummonId, 1, 0);
         tlpAdd.Controls.Add(chkUseAddStage, 2, 0);
         tlpAdd.Controls.Add(lblAddStage, 0, 1);
         tlpAdd.Controls.Add(nudAddStage, 1, 1);
         tlpAdd.Controls.Add(btnAddSummon, 2, 1);
-        tlpAdd.Dock = DockStyle.Fill;
-        tlpAdd.Location = new Point(3, 23);
-        tlpAdd.Name = "tlpAdd";
-        tlpAdd.RowCount = 2;
-        tlpAdd.RowStyles.Add(new RowStyle());
-        tlpAdd.RowStyles.Add(new RowStyle());
-        tlpAdd.Size = new Size(412, 78);
-        tlpAdd.TabIndex = 0;
-        // 
-        // lblSummonId
-        // 
-        lblSummonId.Anchor = AnchorStyles.Left;
-        lblSummonId.AutoSize = true;
-        lblSummonId.Location = new Point(3, 6);
-        lblSummonId.Name = "lblSummonId";
-        lblSummonId.Size = new Size(75, 20);
-        lblSummonId.TabIndex = 0;
-        lblSummonId.Text = "SummonID";
-        // 
+
+        ConfigureLabel(lblSummonId, "SummonID");
+        ConfigureLabel(lblAddStage, "Stage");
+
         // nudSummonId
-        // 
         nudSummonId.Dock = DockStyle.Fill;
-        nudSummonId.Location = new Point(83, 3);
+        nudSummonId.Margin = new Padding(3, 4, 3, 4);
         nudSummonId.Maximum = new decimal(new int[] { 2000000000, 0, 0, 0 });
-        nudSummonId.Name = "nudSummonId";
-        nudSummonId.Size = new Size(93, 27);
-        nudSummonId.TabIndex = 1;
-        // 
+
         // chkUseAddStage
-        // 
         chkUseAddStage.Anchor = AnchorStyles.Left;
-        tlpAdd.SetColumnSpan(chkUseAddStage, 2);
         chkUseAddStage.AutoSize = true;
-        chkUseAddStage.Location = new Point(182, 4);
-        chkUseAddStage.Name = "chkUseAddStage";
-        chkUseAddStage.Size = new Size(99, 24);
-        chkUseAddStage.TabIndex = 2;
         chkUseAddStage.Text = "Use stage";
         chkUseAddStage.UseVisualStyleBackColor = true;
         chkUseAddStage.CheckedChanged += chkUseAddStage_CheckedChanged;
-        // 
-        // lblAddStage
-        // 
-        lblAddStage.Anchor = AnchorStyles.Left;
-        lblAddStage.AutoSize = true;
-        lblAddStage.Location = new Point(3, 40);
-        lblAddStage.Name = "lblAddStage";
-        lblAddStage.Size = new Size(44, 20);
-        lblAddStage.TabIndex = 3;
-        lblAddStage.Text = "Stage";
-        // 
+
         // nudAddStage
-        // 
         nudAddStage.Dock = DockStyle.Fill;
-        nudAddStage.Location = new Point(83, 37);
+        nudAddStage.Margin = new Padding(3, 4, 3, 4);
         nudAddStage.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
-        nudAddStage.Name = "nudAddStage";
-        nudAddStage.Size = new Size(93, 27);
-        nudAddStage.TabIndex = 4;
-        // 
+
         // btnAddSummon
-        // 
-        tlpAdd.SetColumnSpan(btnAddSummon, 2);
-        btnAddSummon.Dock = DockStyle.Fill;
-        btnAddSummon.Location = new Point(182, 37);
-        btnAddSummon.Name = "btnAddSummon";
-        btnAddSummon.Size = new Size(227, 36);
-        btnAddSummon.TabIndex = 5;
         btnAddSummon.Text = "Add Summon";
+        btnAddSummon.AutoSize = false;
+        btnAddSummon.Size = new Size(130, 28);
+        btnAddSummon.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+        btnAddSummon.Margin = new Padding(3);
         btnAddSummon.UseVisualStyleBackColor = true;
         btnAddSummon.Click += btnAddSummon_Click;
-        // 
+
         // gbStageSummon
-        // 
-        gbStageSummon.Controls.Add(tlpStage);
-        gbStageSummon.Dock = DockStyle.Fill;
-        gbStageSummon.Location = new Point(3, 123);
-        gbStageSummon.Name = "gbStageSummon";
-        gbStageSummon.Size = new Size(418, 136);
-        gbStageSummon.TabIndex = 1;
-        gbStageSummon.TabStop = false;
         gbStageSummon.Text = "Stage Summon";
-        // 
+        gbStageSummon.AutoSize = false;
+        gbStageSummon.Padding = new Padding(8, 18, 8, 8);
+        gbStageSummon.Dock = DockStyle.Fill;
+        gbStageSummon.Controls.Add(tlpStage);
+
         // tlpStage
-        // 
+        tlpStage.Dock = DockStyle.Fill;
         tlpStage.ColumnCount = 2;
-        tlpStage.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
+        tlpStage.RowCount = 3;
+        tlpStage.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 130F));
         tlpStage.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+        tlpStage.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+        tlpStage.RowStyles.Add(new RowStyle(SizeType.Absolute, 32F));
+        tlpStage.RowStyles.Add(new RowStyle(SizeType.Absolute, 36F));
         tlpStage.Controls.Add(lblSlot, 0, 0);
         tlpStage.Controls.Add(cmbSlot, 1, 0);
         tlpStage.Controls.Add(lblStage, 0, 1);
         tlpStage.Controls.Add(nudStage, 1, 1);
         tlpStage.Controls.Add(btnStageSummon, 0, 2);
-        tlpStage.Dock = DockStyle.Fill;
-        tlpStage.Location = new Point(3, 23);
-        tlpStage.Name = "tlpStage";
-        tlpStage.RowCount = 3;
-        tlpStage.RowStyles.Add(new RowStyle());
-        tlpStage.RowStyles.Add(new RowStyle());
-        tlpStage.RowStyles.Add(new RowStyle());
-        tlpStage.Size = new Size(412, 110);
-        tlpStage.TabIndex = 0;
-        // 
-        // lblSlot
-        // 
-        lblSlot.Anchor = AnchorStyles.Left;
-        lblSlot.AutoSize = true;
-        lblSlot.Location = new Point(3, 7);
-        lblSlot.Name = "lblSlot";
-        lblSlot.Size = new Size(34, 20);
-        lblSlot.TabIndex = 0;
-        lblSlot.Text = "Slot";
-        // 
+        tlpStage.SetColumnSpan(btnStageSummon, 2);
+
+        ConfigureLabel(lblSlot, "Slot");
+        ConfigureLabel(lblStage, "Stage");
+
         // cmbSlot
-        // 
         cmbSlot.Dock = DockStyle.Fill;
         cmbSlot.DropDownStyle = ComboBoxStyle.DropDownList;
         cmbSlot.FormattingEnabled = true;
         cmbSlot.Items.AddRange(new object[] { 0, 1, 2, 3, 4, 5 });
-        cmbSlot.Location = new Point(93, 3);
-        cmbSlot.Name = "cmbSlot";
-        cmbSlot.Size = new Size(316, 28);
-        cmbSlot.TabIndex = 1;
-        // 
-        // lblStage
-        // 
-        lblStage.Anchor = AnchorStyles.Left;
-        lblStage.AutoSize = true;
-        lblStage.Location = new Point(3, 40);
-        lblStage.Name = "lblStage";
-        lblStage.Size = new Size(44, 20);
-        lblStage.TabIndex = 2;
-        lblStage.Text = "Stage";
-        // 
+        cmbSlot.Margin = new Padding(3, 4, 3, 4);
+
         // nudStage
-        // 
         nudStage.Dock = DockStyle.Fill;
-        nudStage.Location = new Point(93, 37);
+        nudStage.Margin = new Padding(3, 4, 3, 4);
         nudStage.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
-        nudStage.Name = "nudStage";
-        nudStage.Size = new Size(316, 27);
-        nudStage.TabIndex = 3;
-        // 
+
         // btnStageSummon
-        // 
-        tlpStage.SetColumnSpan(btnStageSummon, 2);
-        btnStageSummon.Dock = DockStyle.Fill;
-        btnStageSummon.Location = new Point(3, 70);
-        btnStageSummon.Name = "btnStageSummon";
-        btnStageSummon.Size = new Size(406, 36);
-        btnStageSummon.TabIndex = 4;
         btnStageSummon.Text = "Stage Summon";
+        btnStageSummon.AutoSize = false;
+        btnStageSummon.Size = new Size(130, 28);
+        btnStageSummon.Anchor = AnchorStyles.Top | AnchorStyles.Left;
+        btnStageSummon.Margin = new Padding(3);
         btnStageSummon.UseVisualStyleBackColor = true;
         btnStageSummon.Click += btnStageSummon_Click;
-        // 
+
         // SummonsActionsControl
-        // 
         AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
-        Controls.Add(gbSummonActions);
+        Controls.Add(_tlpRoot);
         MinimumSize = new Size(430, 0);
         Name = "SummonsActionsControl";
-        Size = new Size(430, 260);
-        gbSummonActions.ResumeLayout(false);
-        tlpRoot.ResumeLayout(false);
-        gbAddSummon.ResumeLayout(false);
-        tlpAdd.ResumeLayout(false);
-        tlpAdd.PerformLayout();
-        ((System.ComponentModel.ISupportInitialize)nudSummonId).EndInit();
-        ((System.ComponentModel.ISupportInitialize)nudAddStage).EndInit();
-        gbStageSummon.ResumeLayout(false);
+        Size = new Size(430, 280);
+
+        ((System.ComponentModel.ISupportInitialize)nudStage).EndInit();
         tlpStage.ResumeLayout(false);
         tlpStage.PerformLayout();
-        ((System.ComponentModel.ISupportInitialize)nudStage).EndInit();
+        gbStageSummon.ResumeLayout(false);
+        ((System.ComponentModel.ISupportInitialize)nudAddStage).EndInit();
+        ((System.ComponentModel.ISupportInitialize)nudSummonId).EndInit();
+        tlpAdd.ResumeLayout(false);
+        tlpAdd.PerformLayout();
+        gbAddSummon.ResumeLayout(false);
+        tlpRoot.ResumeLayout(false);
+        gbSummonActions.ResumeLayout(false);
+        _tlpRoot.ResumeLayout(false);
         ResumeLayout(false);
+    }
+
+    private static void ConfigureLabel(Label label, string text)
+    {
+        label.Text = text;
+        label.Dock = DockStyle.Fill;
+        label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+        label.AutoSize = false;
+        label.Margin = new Padding(3, 0, 3, 0);
     }
 }
