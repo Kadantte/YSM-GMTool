8ae97b9d6d49a15386b85446136313894d4d78be
YoSiem <patryk.olejnik655@gmail.com>
2026-05-10 20:19:35 +0200

    feat: wire SqliteConnection in DbConnectionFactory

--- src/App.Data/Infrastructure/DbConnectionFactory.cs
+++ src/App.Data/Infrastructure/DbConnectionFactory.cs
@@ -1,6 +1,7 @@
 using System.Data.Common;
 using App.Core.Enums;
 using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
 using MySqlConnector;
 
 namespace App.Data.Infrastructure;
@@ -18,6 +19,7 @@ public sealed class DbConnectionFactory
         {
             DatabaseProvider.MSSQL => new SqlConnection(connectionString),
             DatabaseProvider.MySQL => new MySqlConnection(connectionString),
+            DatabaseProvider.Sqlite => new SqliteConnection(connectionString),
             _ => throw new ArgumentOutOfRangeException(nameof(provider), provider, "Unsupported database provider.")
         };
     }
